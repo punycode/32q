702c8d0b416ce52d5de9f3f5bfaf9a43c9052ef1
zenhase <zh@punyco.de>
2012-11-01 21:19:17 +0100

    Added findByUserId to UserRepository

--- src/main/java/de/punyco/thirtytwosquare/repository/UserRepository.java
+++ src/main/java/de/punyco/thirtytwosquare/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package de.punyco.thirtytwosquare.repository;
 
 import de.punyco.thirtytwosquare.domain.UserAccount;
+
 import org.springframework.roo.addon.layers.repository.jpa.RooJpaRepository;
+
 
 @RooJpaRepository(domainType = UserAccount.class)
 public interface UserRepository {
+
+    UserAccount findByUserId(String id);
 }
